b38138f6efffe1f14f959f2189d641550dca3df8
dollar-src <78104813+dollar-src@users.noreply.github.com>
2022-05-17 02:31:58 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,8 @@
 Config = {}
 
 
-Config.BlackList = {
-     models = {
-
-     
-    'a_f_y_hipster_02',
-    'a_f_y_hipster_03'
-     }
-}
-
 Config.discord = {
-    ['webhook'] = 'https://discord.com/api/webhooks/956854521357811732/Y6KponlfAvLDk6ykCeRQYla7CqLbk6K6Yw4zP8k5kol03IsYNL3dKZYcmhlbOZDz6PHn',
+    ['webhook'] = 'yourwebhook',
     ['name'] = 'PedMenu',
     ['image'] = 'https://cdn.discordapp.com/avatars/869260464775921675/dea34d25f883049a798a241c8d94020c.png?size=1024'
 }
